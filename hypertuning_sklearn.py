1dc3589b7055d81f958749ff368a1eb60c06d152
Noah Behling <noah.behling@tu-dortmund.de>
2022-06-29 14:19:42 +0200

    included oversampling and reduced search space

--- hypertuning_sklearn.py
+++ hypertuning_sklearn.py
@@ -1,13 +1,14 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split, GridSearchCV
-import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.models import Sequential
 from keras.wrappers.scikit_learn import KerasClassifier
 from tensorflow.compat.v1 import set_random_seed
 import matplotlib.pyplot as plt
 import joblib
+from imblearn.over_sampling import RandomOverSampler
+from imblearn.pipeline import Pipeline
 
 np.random.seed(42)
 set_random_seed(42)
@@ -45,18 +46,18 @@ f = pd.read_csv("data/heart_2020_Female_encoded.csv")
 m = pd.read_csv("data/heart_2020_Male_encoded.csv")
 
 params = {
-            "n_layers": [1, 2, 3],
-            "units": np.linspace(10, 100, 5),
-            "activation": ["relu", "tanh", "selu"],
-            "Dropout": [.1, .2],
-            "learning_rate": np.linspace(1e-3, 1e-2, 5),
+            "model__n_layers": [1, 2, 3, 4, 5],
+            "model__units": [50, 100],
+            "model__activation": ["selu"],  # Better performance than tanh and relu in previous scans
+            "model__Dropout": [.1],
+            "model__learning_rate": [.001, 0.005, .01],
 }
 # params = {
-#             "n_layers": [1],
-#             "units": [10],
-#             "activation": ["relu"],
-#             "Dropout": [.1],
-#             "learning_rate": [1e-3],
+#             "model__n_layers": [1],
+#             "model__units": [10],
+#             "model__activation": ["relu"],
+#             "model__Dropout": [.1],
+#             "model__learning_rate": [1e-3],
 # }
 
 
@@ -73,24 +74,29 @@ for i, data in enumerate([f, m]):
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.3,
                                                         random_state=42)
-    bias = np.log(pos / neg)
+    steps = [('over', RandomOverSampler(sampling_strategy=.5)),
+             ('model', KerasClassifier(build_fn=create_model, epochs=15,
+                                       batch_size=128, verbose=0))]
+    pipeline = Pipeline(steps=steps)
 
-    model = KerasClassifier(build_fn=create_model, epochs=15,
-                            batch_size=128, verbose=0)
-    grid = GridSearchCV(estimator=model, param_grid=params, n_jobs=-1, verbose=1,
+    # model = KerasClassifier(build_fn=create_model, epochs=15,
+    #                         batch_size=128, verbose=0)
+    grid = GridSearchCV(estimator=pipeline, param_grid=params, n_jobs=-1, verbose=1,
                         scoring='recall')
     grid_result = grid.fit(X_train, y_train)
     joblib.dump(grid, f"data/grid_{sex}.pkl")
 
     X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=.2,
                                                       random_state=42)
+    oversample = RandomOverSampler(sampling_strategy=.5, random_state=42)
+    X_over, y_over = oversample.fit_resample(X_train, y_train)
 
-    model = create_model(n_layers=grid.best_params_["n_layers"],
-                         units=grid.best_params_["units"],
-                         activation=grid.best_params_["activation"],
-                         Dropout=grid.best_params_["Dropout"],
-                         learning_rate=grid.best_params_["learning_rate"])
-    history = model.fit(x=X_train, y=y_train, validation_data=(X_val, y_val), epochs=50,
+    model = create_model(n_layers=grid.best_params_["model__n_layers"],
+                         units=grid.best_params_["model__units"],
+                         activation=grid.best_params_["model__activation"],
+                         Dropout=grid.best_params_["model__Dropout"],
+                         learning_rate=grid.best_params_["model__learning_rate"])
+    history = model.fit(x=X_over, y=y_over, validation_data=(X_val, y_val), epochs=50,
                         verbose=2,
                         batch_size=128)
 
